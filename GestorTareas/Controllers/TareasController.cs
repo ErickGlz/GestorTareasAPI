9c56b0abb1b02a55b49b93c67ab7be63a775aa59
garden <killersull15@gmail.com>
2026-05-20 23:15:24 -0600

    ultimos cambios

---
+++ GestorTareas/Controllers/TareasController.cs
@@ -0,0 +1,103 @@
+﻿using FluentValidation;
+using GestorTareas.DTOs.Tareas;
+using GestorTareasAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestorTareasAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TareasController : ControllerBase
+    {
+        private readonly TareasService service;
+
+        public TareasController(TareasService service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet]
+        public IActionResult GetTareas()
+        {
+            var tareas = service.GetAll();
+
+            if (tareas == null || !tareas.Any())
+            {
+                return NotFound();
+            }
+
+            return Ok(tareas);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetTarea(int id)
+        {
+            var tarea = service.GetById(id);
+
+            if (tarea == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(tarea);
+        }
+
+        [HttpPost]
+        public IActionResult CrearTarea(CrearTareaDTO dto)
+        {
+            try
+            {
+                service.Insert(dto);
+
+                return Ok();
+            }
+            catch (ValidationException ex)
+            {
+                return BadRequest(
+                    ex.Errors.Select(x => x.ErrorMessage)
+                );
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public IActionResult EditarTarea(EditarTareaDTO dto)
+        {
+            try
+            {
+                service.Update(dto);
+
+                return Ok();
+            }
+            catch (ValidationException ex)
+            {
+                return BadRequest(
+                    ex.Errors.Select(x => x.ErrorMessage)
+                );
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult EliminarTarea(int id)
+        {
+            try
+            {
+                service.Delete(id);
+
+                return Ok();
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
--- GestorTareas/Program.cs
+++ GestorTareas/Program.cs
@@ -1,5 +1,33 @@
+using FluentValidation;
+using GestorTareas.Models.Entities;
+using GestorTareas.Repositories;
+using GestorTareasAPI.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var cs = builder.Configuration.GetConnectionString("Default");
+
+builder.Services.AddDbContext<RegistroTareasContext>(x =>
+{
+    x.UseMySql(cs, ServerVersion.AutoDetect(cs));
+});
+
+builder.Services.AddScoped(typeof(Repository<>), typeof(Repository<>));
+
+builder.Services.AddScoped<TareasService>();
+
+builder.Services.AddAutoMapper(x =>
+{
+
+}, typeof(Program).Assembly);
+
+builder.Services.AddValidatorsFromAssemblyContaining(typeof(Program));
+
 var app = builder.Build();
 
+app.MapControllers();
 
 app.Run();
